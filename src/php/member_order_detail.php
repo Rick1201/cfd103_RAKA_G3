34c2f9727ac36fb5c297c502062bfb610750de09
izizcrush <eiliimagedesign@gmail.com>
2021-12-28 21:16:58 +0800

    會員頁整合

---
+++ src/php/member_order_detail.php
@@ -0,0 +1,90 @@
+<?php
+$dbname = "rakaa";
+$user = "root";
+$password = "au4a83au4a83666";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=$dbname";
+$connect = new PDO($dsn, $user, $password);
+$dba=file_get_contents("php://input");
+$received_data = json_decode($dba);
+$data= array();
+if($received_data->action =='fetchall'){
+    $query="
+    SELECT * FROM orderdetail O 
+    join product p on o.Product_ID = p.Product_ID
+    WHERE o.Order_ID = '$received_data->id.'
+    ";
+    $statement = $connect->prepare($query);
+    $statement -> execute();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
+        $data[]=$row;
+    }
+    echo json_encode($data);
+}
+if($received_data->action =='insert'){
+   
+    $query = "
+    INSERT INTO manager
+    (Manager_Account, Manager_PSW, Manager_Status)
+    VALUES ('$received_data->managerAcc','$received_data->mangerPsw','$received_data->managerSts')";
+    $statement = $connect -> prepare($query);
+    $statement -> execute();
+    $output = array(
+        'message' => '輸入成功'
+    );
+    echo json_encode($output);
+}
+// if($received_data->action =='fetchSingle'){
+//     $query="
+//     SELECT * FROM manager
+//     WHERE Manager_ID ='$received_data->id.'
+//     ";
+//     $statement= $connect->prepare($query);
+//     $statement->execute();
+//     $result = $statement->fetchAll();
+//     foreach($result as $row){
+//         $data['id'] = $row['Manager_ID'];
+//         $data['Manager_Account']=$row['Manager_Account'];
+//         $data['Manager_PSW']=$row['Manager_PSW'];
+//         $data['Manager_Status']=$row['Manager_Status'];
+
+//     }
+//     echo json_encode ($data);
+// }
+if($received_data->action =='update'){
+$data =array(
+    ':Manager_Account'=>$received_data->managerAcc,
+    ':Manager_PSW'=>$received_data->mangerPsw,
+    ':Manager_Status'=>$received_data->managerSts,
+    ':Manager_ID'=>$received_data->managerId,
+);
+    $query = "
+    UPDATE manager
+    SET Manager_Account = :Manager_Account,
+    Manager_PSW = :Manager_PSW,
+    Manager_Status = :Manager_Status
+    WHERE Manager_ID = :Manager_ID";
+    $statement= $connect->prepare($query);
+    $statement->execute($data);
+    $output = array(
+        'message'=>'修改完成'
+    );
+    echo json_encode($output);
+
+}
+
+if($received_data->action =='delete'){
+    $query = "
+    DELETE FROM manager
+    WHERE Manager_ID = '$received_data->id.'";
+    $statement= $connect->prepare($query);
+    $statement->execute($data);
+    $output = array(
+        'message'=>'已刪除'
+    );
+    echo json_encode($output);
+
+}
+
+?>
