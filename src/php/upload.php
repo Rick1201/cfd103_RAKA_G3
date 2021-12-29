baf2fae9914ef4ad7dedef494d759a89e818efc1
Rick1201 <oops123451@gmail.com>
2021-12-29 20:26:15 +0800

    add css backstage

--- src/php/upload.php
+++ src/php/upload.php
@@ -7,7 +7,7 @@ if(isset($_FILES['file']['name'])){
     $extension = pathinfo($image_name, PATHINFO_EXTENSION);
     if(in_array($extension, $valid_extensions))
     {
-        $upload_path='./images/'.rand(0,9999999999). '.' .$extension;
+        $upload_path='../images/'.rand(0,9999999999). '.' .$extension;
         if(move_uploaded_file($_FILES['file']['tmp_name'],$upload_path))
         {
             $message = '照片上傳';
@@ -25,7 +25,7 @@ else{
 }
 $output = array(
     'message' => $message,
-    'image' => $image
+    'image' => substr($image, 1, mb_strlen($image)-1)
 );
 echo json_encode($output);
 ?>
