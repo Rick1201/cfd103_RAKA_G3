e195150dfe7829f4d4fa8530499cca2f800f7891
Rick1201 <oops123451@gmail.com>
2021-12-30 13:01:15 +0800

    connect to DB PHP

--- src/php/commentManage.php
+++ src/php/commentManage.php
@@ -1,10 +1,5 @@
 <?php
-$dbname = "raka1111";
-$user = "root";
-$password = "wei12345";
-
-$dsn = "mysql:host=localhost;port=3306;dbname=$dbname";
-$connect = new PDO($dsn, $user, $password);
+require_once("connectDB.php");
 $dba=file_get_contents("php://input");
 $received_data = json_decode($dba);
 $data= array();
